574678e55b3af9a778193ff62a1128caf522b0b2
celebibetul <betulcelebi.05@gmail.com>
2022-11-23 10:14:46 +0300

    pages structure created

---
+++ lib/ui/pages/album_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class AlbumScreen extends StatefulWidget {
+  const AlbumScreen({super.key});
+
+  @override
+  State<AlbumScreen> createState() => _AlbumScreenState();
+}
+
+class _AlbumScreenState extends State<AlbumScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
